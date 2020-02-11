589a741035362ac90c8e8b9da06ca5daa8847c2e
Kevenn <k.douglascaterinque@gmail.com>
2020-02-11 14:04:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logado como ${client.user.tag}!`)
+})
+
+client.on('message', msg => {
+  if (msg.content == 'ping')
+    msg.reply('Pong!')
+})
+
+client.login(process.env.BOT_TOKEN)
